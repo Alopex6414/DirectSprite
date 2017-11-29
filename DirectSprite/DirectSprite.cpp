faefb56688b889e8f13c7f1cebf4b133e76178f8
Helium <1029535012@qq.com>
2017-11-29 16:25:09 +0800

    DirectSprite

---
+++ DirectSprite/DirectSprite.cpp
@@ -0,0 +1,329 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017, Alopex/Helium
+*     All rights reserved.
+*
+* @file		DirectSprite.cpp
+* @brief	This File is DirectSprite DLL Project Header.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2017-11-28
+*/
+#include "DirectCommon.h"
+#include "DirectSprite.h"
+
+//D3DXSprite精灵(一般用于2D场景渲染)
+
+//------------------------------------------------------------------
+// @Function:	 DirectSprite()
+// @Purpose: DirectSprite构造函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+DirectSprite::DirectSprite()
+{
+	m_pD3D9Device = NULL;		//IDirect3DDevice9接口指针初始化(NULL)
+	m_pSpriteTexture = NULL;	//ID3DXSprite表面纹理接口指针初始化(NULL)
+	m_pSprite = NULL;			//ID3DXSprite点精灵接口指针初始化(NULL)
+}
+
+//------------------------------------------------------------------
+// @Function:	 ~DirectSprite()
+// @Purpose: DirectSprite析构函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+DirectSprite::~DirectSprite()
+{
+	SAFE_RELEASE(m_pSpriteTexture);	//释放m_pSpriteTexture
+	SAFE_RELEASE(m_pSprite);		//释放m_pSprite
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSprite(LPDIRECT3DDEVICE9 pD3D9Device)
+// @Purpose: DirectSprite构造函数
+// @Since: v1.00a
+// @Para: LPDIRECT3DDEVICE9 pD3D9Device		//Direct3D接口指针
+// @Return: None
+//------------------------------------------------------------------
+DirectSprite::DirectSprite(LPDIRECT3DDEVICE9 pD3D9Device)
+{
+	m_pD3D9Device = pD3D9Device;//IDirect3DDevice9接口指针初始化(传入D3D9设备指针)
+	m_pSpriteTexture = NULL;	//ID3DXSprite表面纹理接口指针初始化(NULL)
+	m_pSprite = NULL;			//ID3DXSprite点精灵接口指针初始化(NULL)
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSpriteInit(LPCWSTR lpszStr)
+// @Purpose: DirectSprite初始化函数
+// @Since: v1.00a
+// @Para: LPCWSTR lpszStr		//精灵纹理路径
+// @Return: None
+//------------------------------------------------------------------
+HRESULT WINAPI DirectSprite::DirectSpriteInit(LPCWSTR lpszStr)
+{
+	VERIFY(D3DXCreateTextureFromFile(m_pD3D9Device, lpszStr, &m_pSpriteTexture));//D3DXSprite精灵加载纹理
+	VERIFY(D3DXCreateSprite(m_pD3D9Device, &m_pSprite));//D3DXSprite精灵创建
+
+	return S_OK;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSpriteReload(LPCWSTR lpszStr)
+// @Purpose: DirectSprite重新加载纹理
+// @Since: v1.00a
+// @Para: LPCWSTR lpszStr		//精灵纹理路径
+// @Return: None
+//------------------------------------------------------------------
+HRESULT WINAPI DirectSprite::DirectSpriteReload(LPCWSTR lpszStr)
+{
+	SAFE_RELEASE(m_pSpriteTexture);	//释放m_pSpriteTexture
+	SAFE_RELEASE(m_pSprite);		//释放m_pSprite
+	VERIFY(D3DXCreateTextureFromFile(m_pD3D9Device, lpszStr, &m_pSpriteTexture));//D3DXSprite精灵加载纹理
+	VERIFY(D3DXCreateSprite(m_pD3D9Device, &m_pSprite));//D3DXSprite精灵创建
+
+	return S_OK;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSpriteBegin(void)
+// @Purpose: DirectSprite开始渲染
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT WINAPI DirectSprite::DirectSpriteBegin(void)
+{
+	VERIFY(m_pSprite->Begin(D3DXSPRITE_ALPHABLEND));
+	return S_OK;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSpriteEnd(void)
+// @Purpose: DirectSprite结束渲染
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+HRESULT WINAPI DirectSprite::DirectSpriteEnd(void)
+{
+	VERIFY(m_pSprite->End());
+	return S_OK;
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSpriteGetTransform(D3DXMATRIX* pMatrix)
+// @Purpose: DirectSprite获取当前变换矩阵
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteGetTransform(D3DXMATRIX* pMatrix)
+{
+	m_pSprite->GetTransform(pMatrix);
+}
+
+//------------------------------------------------------------------
+// @Function:	 DirectSpriteSetTransform(D3DXMATRIX* pMatrix)
+// @Purpose: DirectSprite设置当前变换矩阵
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteSetTransform(D3DXMATRIX* pMatrix)
+{
+	m_pSprite->SetTransform(pMatrix);
+}
+
+//----------------------------------------------------------------------
+// @Function:	 DirectSpriteDraw(DirectSpriteDrawPara* sSpriteDrawPara)
+// @Purpose: DirectSprite设置当前变换矩阵
+// @Since: v1.00a
+// @Para: DirectSpriteDrawPara* sSpriteDrawPara
+// @Return: None
+//----------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDraw(DirectSpriteDrawPara* sSpriteDrawPara)
+{
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDraw(RECT* pSpriteRect, D3DXVECTOR3* pSpriteCenter, D3DXVECTOR3* pSpritePosition, D3DCOLOR SpriteColor)
+// @Purpose: DirectSprite绘制精灵
+// @Since: v1.00a
+// @Para: RECT* pSpriteRect				//绘制矩形区域
+// @Para: D3DXVECTOR3* pSpriteCenter	//绘制中心向量
+// @Para: D3DXVECTOR3* pSpritePosition	//绘制位置向量
+// @Para: D3DCOLOR SpriteColor			//绘制颜色
+// @Return: None
+//----------------------------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDraw(RECT* pSpriteRect, D3DXVECTOR3* pSpriteCenter, D3DXVECTOR3* pSpritePosition, D3DCOLOR SpriteColor)
+{
+	m_pSprite->Draw(m_pSpriteTexture, pSpriteRect, pSpriteCenter, pSpritePosition, SpriteColor);
+}
+
+//---------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawScale(DirectSpriteDrawPara* sSpriteDrawPara, float fScaleX, float fScaleY)
+// @Purpose: DirectSprite绘制拉伸变换
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//---------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawScale(DirectSpriteDrawPara* sSpriteDrawPara, float fScaleX, float fScaleY)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixScale;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixScaling(&MatrixScale, fScaleX, fScaleY, 1.0f);
+	Matrix = Matrix * MatrixScale;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawScale(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTransformPara sScalePara)
+// @Purpose: DirectSprite绘制拉伸变换
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//-----------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawScale(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteScale sScalePara)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixScale;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixScaling(&MatrixScale, sScalePara.fScaleX, sScalePara.fScaleY, 1.0f);
+	Matrix = Matrix * MatrixScale;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawRotate(DirectSpriteDrawPara* sSpriteDrawPara, float fRotateZ)
+// @Purpose: DirectSprite绘制旋转变换
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//-----------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawRotate(DirectSpriteDrawPara* sSpriteDrawPara, float fRotateZ)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixRotate;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixRotationZ(&MatrixRotate, fRotateZ);
+	Matrix = Matrix * MatrixRotate;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawRotate(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteRotate sRotatePara)
+// @Purpose: DirectSprite绘制旋转变换
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//-----------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawRotate(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteRotate sRotatePara)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixRotate;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixRotationZ(&MatrixRotate, sRotatePara.fRotateZ);
+	Matrix = Matrix * MatrixRotate;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawTranslate(DirectSpriteDrawPara* sSpriteDrawPara, float fTranslateX, float fTranslateY)
+// @Purpose: DirectSprite绘制平移变换
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//---------------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawTranslate(DirectSpriteDrawPara* sSpriteDrawPara, float fTranslateX, float fTranslateY)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixTranslate;
+	D3DXMATRIX MatrixInvrsT;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixTranslation(&MatrixTranslate, fTranslateX, fTranslateY, 0.0f);
+	D3DXMatrixInverse(&MatrixInvrsT, NULL, &MatrixTranslate);
+	Matrix = Matrix * MatrixInvrsT * MatrixTranslate;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawTranslate(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTransformPara sTransformPara)
+// @Purpose: DirectSprite绘制平移变换
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//-------------------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawTranslate(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTranslate sTranslatePara)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixTranslate;
+	D3DXMATRIX MatrixInvrsT;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixTranslation(&MatrixTranslate, sTranslatePara.fTranslateX, sTranslatePara.fTranslateY, 0.0f);
+	D3DXMatrixInverse(&MatrixInvrsT, NULL, &MatrixTranslate);
+	Matrix = Matrix * MatrixInvrsT * MatrixTranslate;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+// @Function:	 DirectSpriteDrawTransform(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTransformPara sTransformPara)
+// @Purpose: DirectSprite绘制变换矩阵
+// @Since: v1.00a
+// @Para: D3DXMATRIX* pMatrix
+// @Return: None
+//-------------------------------------------------------------------------------------------------------------------------
+void WINAPI DirectSprite::DirectSpriteDrawTransform(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTransformPara sTransformPara)
+{
+	D3DXMATRIX Matrix;
+	D3DXMATRIX MatrixNative;
+	D3DXMATRIX MatrixScale;
+	D3DXMATRIX MatrixRotate;
+	D3DXMATRIX MatrixTranslate;
+	D3DXMATRIX MatrixInvrsT;
+
+	m_pSprite->GetTransform(&MatrixNative);
+	Matrix = MatrixNative;
+	D3DXMatrixScaling(&MatrixScale, sTransformPara.sScalePara.fScaleX, sTransformPara.sScalePara.fScaleY, 1.0f);
+	D3DXMatrixRotationZ(&MatrixRotate, sTransformPara.sRotatePara.fRotateZ);
+	D3DXMatrixTranslation(&MatrixTranslate, sTransformPara.sTranslatePara.fTranslateX, sTransformPara.sTranslatePara.fTranslateY, 0.0f);
+	D3DXMatrixInverse(&MatrixInvrsT, NULL, &MatrixTranslate);
+	Matrix = Matrix * MatrixInvrsT * MatrixScale * MatrixRotate * MatrixTranslate;
+	m_pSprite->SetTransform(&Matrix);
+	m_pSprite->Draw(m_pSpriteTexture, &(sSpriteDrawPara->SpriteRect), &(sSpriteDrawPara->SpriteCenter), &(sSpriteDrawPara->SpritePosition), sSpriteDrawPara->SpriteColor);
+	m_pSprite->SetTransform(&MatrixNative);
+}
---
+++ DirectSprite/DirectSprite.h
@@ -0,0 +1,91 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017, Alopex/Helium
+*     All rights reserved.
+*
+* @file		DirectSprite.h
+* @brief	This File is DirectSprite DLL Project Header.
+* @author	Alopex/Helium
+* @version	v1.00a
+* @date		2017-11-28
+*/
+#pragma once
+
+#ifndef __DIRECTSPRITE_H_
+#define __DIRECTSPRITE_H_
+
+//Macro Definition
+#ifdef	DIRECTSPRITE_EXPORTS
+#define DIRECTSPRITE_API __declspec(dllexport)
+#else
+#define DIRECTSPRITE_API __declspec(dllimport)
+#endif
+
+//Struct Definition
+typedef struct
+{
+	float fScaleX;	//Scale变换X轴比例	//拉伸
+	float fScaleY;	//Scale变换Y轴比例
+} DirectSpriteScale;
+
+typedef struct
+{
+	float fRotateZ;	//Rotate变换Z轴比例	//旋转
+} DirectSpriteRotate;
+
+typedef struct
+{
+	float fTranslateX;	//Translate变换X轴比例	//平移
+	float fTranslateY;	//Translate变换Y轴比例
+} DirectSpriteTranslate;
+
+//变换参数
+typedef struct
+{
+	DirectSpriteScale sScalePara;			//拉伸
+	DirectSpriteRotate sRotatePara;			//旋转
+	DirectSpriteTranslate sTranslatePara;	//平移
+} DirectSpriteTransformPara;
+
+//绘制参数
+typedef struct
+{
+	RECT SpriteRect;						//Sprite矩形区域
+	D3DXVECTOR3 SpriteCenter;				//Sprite中心向量
+	D3DXVECTOR3 SpritePosition;				//Sprite位置向量
+	D3DCOLOR SpriteColor;					//Sprite颜色
+} DirectSpriteDrawPara;
+
+//Class Definition
+class DIRECTSPRITE_API DirectSprite
+{
+protected:
+	LPDIRECT3DDEVICE9 m_pD3D9Device;		//The Direct3D 9 Render Device
+	LPDIRECT3DTEXTURE9 m_pSpriteTexture;	//The Direct3D 9 Sprite Texture
+	LPD3DXSPRITE m_pSprite;					//The Direct3D 9 Sprite
+
+private:
+
+public:
+	DirectSprite();				//DirectSprite Constructor Function
+	virtual ~DirectSprite();	//DirectSprite Destructor Function
+
+	DirectSprite(LPDIRECT3DDEVICE9 pD3D9Device);					//DirectSprite Constructor Function(Use D3D Device)
+	virtual HRESULT WINAPI DirectSpriteInit(LPCWSTR lpszStr);		//DirectSprite Initialize
+	virtual HRESULT WINAPI DirectSpriteReload(LPCWSTR lpszStr);		//DirectSprite Reload(Texture Changed)
+	HRESULT WINAPI DirectSpriteBegin(void);							//DirectSprite Begin Draw
+	HRESULT WINAPI DirectSpriteEnd(void);							//DirectSprite End Draw
+	void WINAPI DirectSpriteGetTransform(D3DXMATRIX* pMatrix);		//DirectSprite Get Transform
+	void WINAPI DirectSpriteSetTransform(D3DXMATRIX* pMatrix);		//DirectSprite Set Transform
+	void WINAPI DirectSpriteDraw(DirectSpriteDrawPara* sSpriteDrawPara);																//DirectSprite Draw
+	void WINAPI DirectSpriteDraw(RECT* pSpriteRect, D3DXVECTOR3* pSpriteCenter, D3DXVECTOR3* pSpritePosition, D3DCOLOR SpriteColor);	//DirectSprite Draw
+	void WINAPI DirectSpriteDrawScale(DirectSpriteDrawPara* sSpriteDrawPara, float fScaleX = 1.0f, float fScaleY = 1.0f);				//DirectSprite Draw Scale
+	void WINAPI DirectSpriteDrawScale(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteScale sScalePara); 							//DirectSprite Draw Scale
+	void WINAPI DirectSpriteDrawRotate(DirectSpriteDrawPara* sSpriteDrawPara, float fRotateZ = 0.0f);									//DirectSprite Draw Rotate
+	void WINAPI DirectSpriteDrawRotate(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteRotate sRotatePara);							//DirectSprite Draw Rotate
+	void WINAPI DirectSpriteDrawTranslate(DirectSpriteDrawPara* sSpriteDrawPara, float fTranslateX = 0.0f, float fTranslateY = 0.0f);	//DirectSprite Draw Translate
+	void WINAPI DirectSpriteDrawTranslate(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTranslate sTranslatePara);					//DirectSprite Draw Translate
+	void WINAPI DirectSpriteDrawTransform(DirectSpriteDrawPara* sSpriteDrawPara, DirectSpriteTransformPara sTransformPara);				//DirectSprite Draw Transform
+};
+
+#endif
